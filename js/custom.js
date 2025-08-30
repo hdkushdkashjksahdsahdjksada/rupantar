fceb6c7717ee5239eb27ff9e0b3301e429ca9ca6
Arpan Mukherjee <arpan.mukh@gmail.com>
2025-08-31 02:01:39 +0530

    blogs, js errors, geo

--- js/custom.js
+++ js/custom.js
@@ -217,74 +217,83 @@ $(function () {
 
 	/* Contact-form
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	$.validator.setDefaults({
-		submitHandler: function () {
-			alert("submitted!");
-		}
-	});
-
-	$(document).ready(function () {
-		$("#contact-form").validate({
-			rules: {
-				firstname: "required",
-				email: {
-					required: true,
-					email: true
-				},
-				lastname: "required",
-				message: "required",
-				agree: "required"
-			},
-			messages: {
-				firstname: "Please enter your firstname",
-				email: "Please enter a valid email address",
-				lastname: "Please enter your lastname",
-				username: {
-					required: "Please enter a username",
-					minlength: "Your username must consist of at least 2 characters"
-				},
-				message: "Please enter your Message",
-				agree: "Please accept our policy"
-			},
-			errorElement: "div",
-			errorPlacement: function (error, element) {
-				// Add the `help-block` class to the error element
-				error.addClass("help-block");
-
-				if (element.prop("type") === "checkbox") {
-					error.insertAfter(element.parent("input"));
-				} else {
-					error.insertAfter(element);
-				}
-			},
-			highlight: function (element, errorClass, validClass) {
-				$(element).parents(".col-md-4, .col-md-12").addClass("has-error").removeClass("has-success");
-			},
-			unhighlight: function (element, errorClass, validClass) {
-				$(element).parents(".col-md-4, .col-md-12").addClass("has-success").removeClass("has-error");
+	// Check if jQuery Validator is loaded before using it
+	if (typeof $.validator !== 'undefined') {
+		$.validator.setDefaults({
+			submitHandler: function () {
+				alert("submitted!");
 			}
 		});
-	});
+	}
+
+	// Check if jQuery Validator is loaded before using contact form validation
+	if (typeof $.validator !== 'undefined') {
+		$(document).ready(function () {
+			$("#contact-form").validate({
+				rules: {
+					firstname: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					lastname: "required",
+					message: "required",
+					agree: "required"
+				},
+				messages: {
+					firstname: "Please enter your firstname",
+					email: "Please enter a valid email address",
+					lastname: "Please enter your lastname",
+					username: {
+						required: "Please enter a username",
+						minlength: "Your username must consist of at least 2 characters"
+					},
+					message: "Please enter your Message",
+					agree: "Please accept our policy"
+				},
+				errorElement: "div",
+				errorPlacement: function (error, element) {
+					// Add the `help-block` class to the error element
+					error.addClass("help-block");
+
+					if (element.prop("type") === "checkbox") {
+						error.insertAfter(element.parent("input"));
+					} else {
+						error.insertAfter(element);
+					}
+				},
+				highlight: function (element, errorClass, validClass) {
+					$(element).parents(".col-md-4, .col-md-12").addClass("has-error").removeClass("has-success");
+				},
+				unhighlight: function (element, errorClass, validClass) {
+					$(element).parents(".col-md-4, .col-md-12").addClass("has-success").removeClass("has-error");
+				}
+			});
+		});
+	}
 
 	/* heroslider
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
-	var swiper = new Swiper('.heroslider', {
-		spaceBetween: 30,
-		centeredSlides: true,
-		slidesPerView: 'auto',
-		paginationClickable: true,
-		loop: true,
-		autoplay: {
-			delay: 2500,
-			disableOnInteraction: false,
-		},
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			dynamicBullets: true
-		},
-	});
+	// Check if Swiper is loaded before using it
+	if (typeof Swiper !== 'undefined') {
+		var swiper = new Swiper('.heroslider', {
+			spaceBetween: 30,
+			centeredSlides: true,
+			slidesPerView: 'auto',
+			paginationClickable: true,
+			loop: true,
+			autoplay: {
+				delay: 2500,
+				disableOnInteraction: false,
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+				dynamicBullets: true
+			},
+		});
+	}
 
 
 	/* Product Filters
@@ -341,81 +350,100 @@ $(function () {
 	/* Deal Slider
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
-	$('.deal-slider').slick({
-		dots: false,
-		infinite: false,
-		prevArrow: '.previous-deal',
-		nextArrow: '.next-deal',
-		speed: 500,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		infinite: false,
-		responsive: [{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 2,
-				infinite: true,
-				dots: false
-			}
-		}, {
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
-			}
-		}, {
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}]
-	});
+	// Check if Slick is loaded before using it
+	if (typeof $.fn.slick !== 'undefined') {
+		$('.deal-slider').slick({
+			dots: false,
+			infinite: false,
+			prevArrow: '.previous-deal',
+			nextArrow: '.next-deal',
+			speed: 500,
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: false,
+			responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 2,
+					infinite: true,
+					dots: false
+				}
+			}, {
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			}, {
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}]
+		});
+	}
 
 	/* News Slider
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
-	$('#news-slider').slick({
-		dots: false,
-		infinite: false,
-		prevArrow: '.previous',
-		nextArrow: '.next',
-		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		responsive: [{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				infinite: true,
-				dots: false
-			}
-		}, {
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}, {
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		}]
-	});
+	// Check if Slick is loaded before using it
+	if (typeof $.fn.slick !== 'undefined') {
+		$('#news-slider').slick({
+			dots: false,
+			infinite: false,
+			prevArrow: '.previous',
+			nextArrow: '.next',
+			speed: 500,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			responsive: [{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: false
+				}
+			}, {
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}, {
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}]
+		});
+	}
 
 	/* Fancybox
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
-	$(".fancybox").fancybox({
-		maxWidth: 1200,
-		maxHeight: 600,
-		width: '70%',
-		height: '70%',
-	});
+	// Check if Fancybox is loaded before using it
+	if (typeof $.fn.fancybox !== 'undefined') {
+		$(".fancybox").fancybox({
+			// Fancybox v3.x options
+			width: '70%',
+			height: '70%',
+			maxWidth: 1200,
+			maxHeight: 600,
+			animationEffect: "fade",
+			transitionEffect: "slide",
+			toolbar: true,
+			buttons: [
+				"slideShow",
+				"fullScreen",
+				"thumbs",
+				"close"
+			]
+		});
+	}
 
 	/* Toggle sidebar
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
